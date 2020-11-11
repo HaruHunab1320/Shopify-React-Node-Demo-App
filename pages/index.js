8ff10c3baa8e4c642504419ee980036ac2a6ffa2
Jakob <jakobmsgrant@gmail.com>
2020-11-10 16:56:35 -0800

    Build out emptystate, layout and page in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import { EmptyState, Layout, Page } from "@shopify/polaris";
+
+const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
+
 const Index = () => (
-  <div>
-    <p>Sample app using React and Next.js</p>
-  </div>
+  <Page>
+    <Layout>
+      {/* Polaris Empty state component helps to communicate the value of your app and its primary action when merchants first add it to their Shopify admin. */}
+      <EmptyState
+        heading="Discount your products temporarily"
+        action={{
+          content: "Select Products",
+          onAction: () => console.log("clicked"),
+        }}
+        image={img}
+      >
+        Sample app using React and Next.js
+      </EmptyState>
+    </Layout>
+  </Page>
 );
 
 export default Index;
