5683ebbfdfce4a4f7c3ead0cc47faaea00965c30
Jakob <jakobmsgrant@gmail.com>
2020-11-11 21:33:38 -0800

    provider config

--- pages/_app.js
+++ pages/_app.js
@@ -10,17 +10,24 @@ import "@shopify/polaris/dist/styles.css";
 
 // Translations are required for AppProvider
 import translations from "@shopify/polaris/locales/en.json";
+import Cookies from "js-cookie";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
+    //config to pass to provider component
+    const config = {
+      apiKey: API_KEY,
+      shopOrigin: Cookies.get("shopOrigin"),
+      forceRedirect: true,
+    };
     return (
       <>
         <Head>
           <title>Sample App</title>
           <meta charSet="utf-8" />
         </Head>
-        <Provider>
+        <Provider config={config}>
           {/* Polaris AppProvider component passes down the props and context */}
           {/* When using Polaris, you are able to import translations from all languages supported by the core Shopify product and consume them through the i18n prop. */}
           <AppProvider i18n={translations}>
