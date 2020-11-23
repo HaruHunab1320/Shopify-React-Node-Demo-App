0df620c33dd2c21d1a2df388ac3769cbd5142541
Jakob <jakobmsgrant@gmail.com>
2020-11-23 15:52:28 -0800

    added getSubscriptionUrl billing api mutation

--- pages/edit-products.js
+++ pages/edit-products.js
@@ -5,7 +5,7 @@ import {
   DisplayText,
   Form,
   FormLayout,
-  Frame
+  Frame,
   Layout,
   Page,
   PageActions,
@@ -49,70 +49,68 @@ class EditProducts extends React.Component {
         {(handleSubmit, { error, data }) => {
           const showError = error && (
             <Banner status="critical">{error.message}</Banner>
-          )
+          );
           const showToast = data && data.productVariantUpdate && (
-            <Toast 
-            content="Successfully updated"
-            onDismiss={() => this.setState({ showToast: false})}
+            <Toast
+              content="Successfully updated"
+              onDismiss={() => this.setState({ showToast: false })}
             />
-          )
+          );
           return (
             <Frame>
-            <Page>
-              <Layout>
-                {showToast}
-                <Layout.Section>
-                  {showError}
-                </Layout.Section>
-                <Layout.Section>
-                  <DisplayText size="large">{name}</DisplayText>
-                  <Form>
-                    <Card sectioned>
-                      <FormLayout>
-                        <FormLayout.Group>
-                          <TextField
-                            prefix="$"
-                            value={price}
-                            disabled={true}
-                            label="Original price"
-                            type="price"
-                          />
-                          <TextField
-                            prefix="$"
-                            value={discount}
-                            onChange={this.handleChange("discount")}
-                            label="Discounted price"
-                            type="discount"
-                          />
-                        </FormLayout.Group>
-                        <p>This sale price will expire in two weeks</p>
-                      </FormLayout>
-                    </Card>
-                    <PageActions
-                      primaryAction={[
-                        {
-                          content: "Save",
-                          onAction: () => {
-                            const productVariableInput = {
-                              id: variantId,
-                              price: discount,
-                            };
-                            handleSubmit({
-                              variables: { input: productVariableInput },
-                            });
+              <Page>
+                <Layout>
+                  {showToast}
+                  <Layout.Section>{showError}</Layout.Section>
+                  <Layout.Section>
+                    <DisplayText size="large">{name}</DisplayText>
+                    <Form>
+                      <Card sectioned>
+                        <FormLayout>
+                          <FormLayout.Group>
+                            <TextField
+                              prefix="$"
+                              value={price}
+                              disabled={true}
+                              label="Original price"
+                              type="price"
+                            />
+                            <TextField
+                              prefix="$"
+                              value={discount}
+                              onChange={this.handleChange("discount")}
+                              label="Discounted price"
+                              type="discount"
+                            />
+                          </FormLayout.Group>
+                          <p>This sale price will expire in two weeks</p>
+                        </FormLayout>
+                      </Card>
+                      <PageActions
+                        primaryAction={[
+                          {
+                            content: "Save",
+                            onAction: () => {
+                              const productVariableInput = {
+                                id: variantId,
+                                price: discount,
+                              };
+                              handleSubmit({
+                                variables: { input: productVariableInput },
+                              });
+                            },
                           },
-                        },
-                      ]}
-                      secondaryActions={[
-                        {
-                          content: "Remove discount",
-                        },
-                      ]}
-                    />
-                  </Form>
-                </Layout.Section>
-              </Layout>
-            </Page>
+                        ]}
+                        secondaryActions={[
+                          {
+                            content: "Remove discount",
+                          },
+                        ]}
+                      />
+                    </Form>
+                  </Layout.Section>
+                </Layout>
+              </Page>
             </Frame>
           );
         }}
